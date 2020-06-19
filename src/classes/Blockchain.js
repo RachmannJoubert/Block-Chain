9a042def786db14eb6e17bc3cfbca690659d6602
Rachmann Joubert <rachmannjoubert@gmail.com>
2020-06-19 20:45:46 +0100

    Created addBlock method

--- src/classes/Blockchain.js
+++ src/classes/Blockchain.js
@@ -1,3 +1,5 @@
+import Block from './Block.js';
+
 class Blockchain {
     
     constructor() {
@@ -15,7 +17,14 @@ class Blockchain {
         return new Block(0, currentDate, "Genisis Block", "0");
     }
 
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 }
+
