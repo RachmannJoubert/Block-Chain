ed52c6f334fa60c7ed37ddadad2b61a804d76a9b
Rachmann Joubert <rachmannjoubert@gmail.com>
2020-06-20 14:02:03 +0100

    Corrected issue with hash not being a string

--- src/classes/Block.js
+++ src/classes/Block.js
@@ -25,7 +25,7 @@ class Block {
     }
 
     mineBlock(difficulty) {
-        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+        while(this.hash.toString().substr(0, difficulty) !== Array(difficulty + 1).join("0")) {
             this.nonce++;
             this.hash = this.calculateHash();
         }
