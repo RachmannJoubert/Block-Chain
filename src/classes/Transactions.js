fa9c0bc03ae16e29961ae1c95fb97577309cd875
Rachmann Joubert <rachmannjoubert@gmail.com>
2020-06-20 16:14:33 +0100

    Created method to sign transactions

--- src/classes/Transactions.js
+++ src/classes/Transactions.js
@@ -1,11 +1,26 @@
 const { Block } = require('./Block');
 const { Blockchain } = require('./Blockchain');
+const { SHA256 } = require('crypto-js');
 
 class Transactions {
     constructor(fromAddress, toAddress, amount) {
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
+
+    calculateHash() {
+        return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+    }
+
+    signTransaction(signingKey) {
+        if(signingKey.getPublic('hex') !== this.fromAddress) {
+            throw new Error('Public key does not match!')
+        }
+
+        const hashTx = this.calculateHash();
+        const sig = signingKey.sign(hashTx, 'base64');
+        this.signature = sig.toDER('hex'); 
     }
 }
 
