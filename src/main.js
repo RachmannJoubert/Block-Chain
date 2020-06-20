a3050994d9054bf57808e390f40c540435b8a6ca
Rachmann Joubert <rachmannjoubert@gmail.com>
2020-06-20 14:03:17 +0100

    Implemented tests

--- src/main.js
+++ src/main.js
@@ -3,18 +3,10 @@ const { Block } = require('./classes/Block');
 
 let RC = new Blockchain();
 
+console.log("Mining Block...")
 RC.addBlock(new Block(1, this.setDate, { amount: 10 }));
+console.log("Mining Block...")
+RC.addBlock(new Block(2, this.setDate, { amount: 15 }));
+console.log("Mining Block...")
+RC.addBlock(new Block(3, this.setDate, { amount: 20 }));
 
-// if(RC.validateChain() == true) {
-//     console.log("Blockchain is valid" + JSON.stringify(RC, null, 4))
-// } else {
-//     console.log("Blockchain is not valid!")
-// }
-
-RC.chain[1].data = { amount: 100};
-
-if(RC.validateChain() == true) {
-    console.log("Blockchain is valid" + JSON.stringify(RC, null, 4))
-} else {
-    console.log("Blockchain is not valid!")
-}
