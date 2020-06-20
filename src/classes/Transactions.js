11cc8767b6a758fce9172db971bb78691a85b6d6
Rachmann Joubert <rachmannjoubert@gmail.com>
2020-06-20 15:51:28 +0100

    Created and exported Transcations class

--- src/classes/Transactions.js
+++ src/classes/Transactions.js
@@ -1,2 +1,12 @@
 const { Block } = require('./Block');
 const { Blockchain } = require('./Blockchain');
+
+class Transactions {
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
+module.exports.Transaction = Transactions;
