9fe0767004f94213c9b78f6bfb1c4acb62b032a9
Rachmann Joubert <rachmannjoubert@gmail.com>
2020-06-20 15:53:13 +0100

    Implemented tests

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 const { Blockchain } = require('./classes/Blockchain');
 const { Block } = require('./classes/Block');
+const { Transaction } = require('./classes/Transactions');
 
 let RC = new Blockchain();
+ RC.createTransaction(new Transaction('address1', 'address2', 100))
+ RC.createTransaction(new Transaction('address2', 'address1', 50))
 
+ console.log('Starting Miner...');
+ RC.minePendingTransactions("Reward address");
 
+ console.log("Balance of account is: ", RC.getAddressBalance('Reward address'))
+
+ RC.minePendingTransactions("Reward address");
+
+ console.log("Balance of account is: ", RC.getAddressBalance('Reward address'))
